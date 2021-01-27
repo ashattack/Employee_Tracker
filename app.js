c1dbe2a2a94f4a33a97c52dc2526c4573c168b44
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-01-27 15:42:48 -0500

    added viewEmployees function to view all in table

--- app.js
+++ app.js
@@ -6,6 +6,41 @@ require("console.table");
 function init() {
     const logoText = logo({ name: "Ashlyn's Employee Manager" }).render();
     console.log(logoText);
+    loadMainMenu();
+}
+
+function loadMainMenu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                }
+            ]
+
+        }
+    ]).then(function (choices) {
+        console.log(choices);
+        handleChoices(choices);
+
+    })
+}
+function handleChoices(choices) {
+    switch (choices.choice) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees()
+
+    }
+}
+
+async function viewEmployees() {
+    const employees = await db.findAllEmployees()
+    console.log("\n");
+    console.table(employees);
 }
 
 init()
